78de5df61e1f46a083a73782c52ec8878b78f4e1
Julia Ch <juliecha@yandex.ru>
2022-12-17 21:02:48 +0300

    UPD for, try, while

--- for.py
+++ for.py
@@ -1,3 +1,4 @@
+# исключения while True
 def input_day():
     while True:
         try:
@@ -9,4 +10,31 @@ def input_day():
         except:
             print('Ну просили же ввести цифру, а ты?')
 
-week_day = input_day()
+# week_day = input_day()
+
+
+# for
+my_list = [22, 52, 54, 72, 7, 80]
+
+for item in my_list:
+    if item < 10:
+        print(item)
+        break
+else:
+    print('Однозначных чисел в списке нет')
+
+print()
+for item in range(len(my_list)):
+    print(item)
+
+print()
+for i in range(1, 10, 2): # range не перебирает индексы, а генерирует последовательность (и мы им можем пользоваться как перебором индексов)
+    print(i)
+
+print()
+
+# while 
+i = 0
+while i <10: # можено перебирать бесконечно )))
+    print(i)
+    i+=1
