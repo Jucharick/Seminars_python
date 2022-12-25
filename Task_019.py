9aac4ae81d8ba9a0ae9caa3264c48b941c819442
Julia Ch <juliecha@yandex.ru>
2022-12-25 13:31:54 +0300

    Add Task_019 - add map

--- Task_019.py
+++ Task_019.py
@@ -4,11 +4,16 @@
 
 nums = input('Введите числа через пробел: ').split(' ')
 print(nums)
-max = int(nums[0])
-min = int(nums[0])
+max_n = int(nums[0])
+min_n = int(nums[0])
 for i in range(len(nums)):
-    if int(nums[i]) > max:
-        max = int(nums[i])
-    if int(nums[i]) < min:
-        min = int(nums[i])
-print(f'Большее => {max}, меньшее => {min}')
+    if int(nums[i]) > max_n:
+        max_n = int(nums[i])
+    if int(nums[i]) < min_n:
+        min_n = int(nums[i])
+print(f'Большее => {max_n}, меньшее => {min_n}')
+
+# использование map
+list_num = list(map(int, nums)) # map итерирует nums и пременяет метод int к каждому элементу nums
+print(list_num)
+print(f'Большее => {max(list_num)}, меньшее => {min(list_num)}')
