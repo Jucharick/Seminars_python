bfadcff012a43afa19492e935449a147b9c28346
Julia Ch <juliecha@yandex.ru>
2022-12-25 13:04:41 +0300

    Add Task_019 - find min & max

---
+++ Task_019.py
@@ -0,0 +1,14 @@
+# Задайте строку из набора чисел.
+# Напишите программу, которая покажет большее и меньшее число.
+# В качестве символа разделителя используйте пробел.
+
+nums = input('Введите числа через пробел: ').split(' ')
+print(nums)
+max = int(nums[0])
+min = int(nums[0])
+for i in range(len(nums)):
+    if int(nums[i]) > max:
+        max = int(nums[i])
+    if int(nums[i]) < min:
+        min = int(nums[i])
+print(f'Большее => {max}, меньшее => {min}')
